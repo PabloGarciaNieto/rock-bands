401fb80adf29df3138ecc9e8aeab3cdfb85ddec2
Pablo Garcia <polteus2001@gmail.com>
2021-05-21 11:38:28 +0200

    inicio de la app

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//MATERIAL MODULES-------------------
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatButtonModule} from '@angular/material/button';
+//COMPONENTS--------------
+import { AppComponent } from './app.component';
+import { BandsTableComponent } from './table/bands-table/bands-table.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    BandsTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/interface-band.ts
@@ -0,0 +1,8 @@
+export interface Band {
+  id: string;
+  name: string;
+  country: string;
+  members: string[];
+ /*  albums: string[];
+  song: string; */
+}
